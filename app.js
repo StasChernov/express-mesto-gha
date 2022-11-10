02c57ecf630dff0d70391be7dffe80ad89363ece
Stas Chernov <schernovmail@yandex.ru>
2022-11-10 15:26:08 +0300

    Практическая работа 13 версия 1

--- app.js
+++ app.js
@@ -19,7 +19,12 @@ app.use((req, res, next) => {
   next();
 });
 
+app.listen(PORT);
+
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.listen(PORT);
+app.use((req, res, next) => {
+  res.status(404).send({ message: 'Страница не найдена.' });
+  next();
+});
