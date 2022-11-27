5bf214655a4f428324f70045da07ec2e37b3e331
Stas Chernov <schernovmail@yandex.ru>
2022-11-27 21:52:44 +0300

    Практическая работа 14 версия 1

--- constants/constants.js
+++
@@ -1,2 +0,0 @@
-const regex = /^(https?:\/\/)(www\.)?([a-z1-9-]{2,}\.)+[a-z]{2,}\/?[a-z0-9-._~:/?#[\]@!$&'()*+,;=]*/i;
-exports.regex = regex;
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUsers,
@@ -10,8 +11,21 @@ const {
 
 router.get('/me', getUserMe);
 router.get('/', getUsers);
-router.get('/:userId', getUserById);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24).required(),
+  }),
+}), getUserById);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUser);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().uri().regex(/(https?:\/\/)([www.]?[a-zA-Z0-9-]+\.)([^\s]{2,})/).required(),
+  }),
+}), updateAvatar);
 
 module.exports = router;
