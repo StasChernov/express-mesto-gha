3a1472b7565a2d7f66b7b8a209bf912b54fc51d6
Stas Chernov <schernovmail@yandex.ru>
2022-11-27 17:21:01 +0300

    Практическая работа 14 версия 1

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   const { statusCode = 500, message } = err;
   res
     .status(statusCode)
