24e8d47eb36a165d8d67f6dfa31f12defb311333
Stas Chernov <schernovmail@yandex.ru>
2022-11-28 08:56:18 +0300

    fix: fix bugs after review

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 const { celebrate, Joi, errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const handlerErrors = require('./middlewares/handlerErrors');
+const NotFoundError = require('./errors/not-found-err');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(helmet());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -37,22 +40,8 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use((req, res, next) => {
-  res.status(404).send({ message: 'Страница не найдена.' });
-  next();
-});
-
+app.use((req, res, next) => next(new NotFoundError('Страница не найдена')));
 app.use(errors());
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-  next();
-});
+app.use(handlerErrors);
 
 app.listen(PORT);
